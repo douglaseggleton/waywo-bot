67a5ebfd950cb2022f71192dba93667cf123d1b4
Douglas Eggleton <douglas.eggleton@outlook.com>
2019-08-29 16:21:07 -0400

    Updating bot

--- index.js
+++ index.js
@@ -8,15 +8,14 @@ const generateMessage = (user) => `
   BOOM BOOM BOOM LET ME HEAR YOU SAY WAYWO, WAYWO! Who’s up you ask?! <@${user}> is up! Show us what you’re working on!
 `;
 
-exports.handler = (event, context, callback) => {
-  slack.channels.info({
+exports.handler = async () => {
+  await slack.channels.info({
     channel,
     token
   }).then((res) => {
     const members = res.channel.members;
-    const randomIndex = random.int(min = 0, max = members.length);
+    const randomIndex = random.int(0, members.length - 1);
     return res.channel.members[randomIndex];
   })
-  .then((user) => slack.chat.postMessage({token, channel, text: generateMessage(user)}))
-  .then(callback);
+  .then((user) => slack.chat.postMessage({token, channel, text: generateMessage(user)}));
 }
